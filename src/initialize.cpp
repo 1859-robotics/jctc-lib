498ffabe9a7e239e28892cb96e2fd78d1848ef01
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-02-11 15:21:01 -0500

    remove okapi

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 
+// only here to not make pros mad and allow for compilation
+
 void initialize() {}
 
 void disabled() {}
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,3 +1,5 @@
 #include "main.h"
 
+// only here to not make pros mad and allow for compilation
+
 void opcontrol() {}
