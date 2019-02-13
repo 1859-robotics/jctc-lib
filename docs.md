0611e19411cbb2d28f02513f7c42edb7c6ea4aa6
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-02-13 15:42:25 -0500

    everything hurts

---
+++ docs.md
@@ -0,0 +1,18 @@
+# How to do shit
+
+## Build
+
+```
+prosv5 make template
+```
+
+## Publish
+
+```
+prosv5 c f jctclib@0.0.1.zip
+```
+
+## Add to pros
+```
+prosv5 c a jctclib
+```
--- jctc.hpp
+++ jctc.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "./include/jctc/misc/misc.hpp"
-#include "./include/jctc/pid/pid.hpp"
-#include "./include/jctc/odom/odom.hpp"
-#include "./include/jctc/odom/position.hpp"
-#include "./include/jctc/chassis/chassis.hpp"
-#include "./include/jctc/logger/logger.hpp"
+#include "jctc/misc/misc.hpp"
+#include "jctc/pid/pid.hpp"
+#include "jctc/odom/odom.hpp"
+#include "jctc/odom/position.hpp"
+#include "jctc/chassis/chassis.hpp"
+#include "jctc/logger/logger.hpp"
 
 using namespace jctc;
